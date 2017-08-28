e3ccaa2464ed4a62548474323a95a7d2e3673cc6
fantasygamery <30944486+fantasygamery@users.noreply.github.com>
2017-08-28 17:10:56 -0500

    Update JKelvin.js

--- JKelvin/JKelvin.js
+++ JKelvin/JKelvin.js
@@ -1,4 +1,4 @@
-function e(asd,need = true){
+module.exports=function(asd,need = true){
     if (asd === undefined || asd === null && need) {return console.log('Specify element');}
     else if (asd !== null){ 
     if (asd.substring(0,1)=='#'){
@@ -33,6 +33,17 @@ function e(asd,need = true){
     catch(z){console.log(z);}
     };
     
+    this.log = function(ss){
+        console.log(ss)
+    };
+ 
+    this.get = function(url){
+        var ht = new XMLHttpRequest();
+        ht.open("GET",url,false);
+        ht.send(null);
+        return ht.responseText;      
+    };
+
     return this;
     }
     
